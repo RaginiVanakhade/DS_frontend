982f60af40ac18ef74025dad65b515e0179ab758
Ragini Vanakhade <raginivankhade1203@gmail.com>
2026-07-02 21:40:26 +0530

    added login register component

--- DigitalSalt/src/App.jsx
+++ DigitalSalt/src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
 import { Routes, Route, Link } from "react-router-dom";
+import Login from "./auth/Login"
+import Register from "./auth/Register"
 
 function App() {
   return (
     <>
         <Routes>
-        <Route path="/" element={"home"} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/about" element={"about"} />
         <Route path="/contact" element={"contact"} />
       </Routes>
---
+++ DigitalSalt/src/auth/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+    <div>
+        <form>
+            <input type="email" placeholder="Enter Email" />
+            <input type="password" placeholder="Enter Password" />
+            <button type="submit">Login</button>
+        </form>
+      
+    </div>
+    </>
+  )
+}
+
+export default Login
