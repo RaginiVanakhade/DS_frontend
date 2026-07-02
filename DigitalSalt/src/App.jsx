46635b081e97a2eb9a7d054ffd16ebca930bb5e5
Ragini Vanakhade <raginivankhade1203@gmail.com>
2026-07-02 22:23:48 +0530

    added forgot and reset component

--- DigitalSalt/src/App.jsx
+++ DigitalSalt/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { Routes, Route, Link } from "react-router-dom";
 import Login from "./auth/Login"
 import Register from "./auth/Register"
+import ForgotPass from "./auth/ForgotPass"
+import ResetPass from "./auth/ResetPass"
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPass />} />
+        <Route path="/reset-password" element={<ResetPass />} />
         <Route path="/about" element={"about"} />
         <Route path="/contact" element={"contact"} />
       </Routes>
